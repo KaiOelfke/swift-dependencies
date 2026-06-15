KaiOelfke/swift-dependencies

commit 411297072220a96f1501211afebd3715316907d2

Improve `#Preview` app entry point detection (#412)
Currently we detect this deep in `DependencyValues`, but we can localize
it in `prepareDependencies` instead and avoid potential preview crashes.

Sources/Dependencies/DependencyValues.swift
@@ -284,11 +284,6 @@ public struct DependencyValues: Sendable {
     }
     set {
       if DependencyValues.isPreparing {
-        #if canImport(SwiftUI)
-          if context == .preview, Thread.isPreviewAppEntryPoint {
-            return
-          }
-        #endif
         cachedValues.lock.lock()
         defer { cachedValues.lock.unlock() }
         let cacheKey = CachedValues.CacheKey(id: TypeIdentifier(key), context: context)
@@ -579,11 +574,6 @@ public final class CachedValues: @unchecked Sendable {
         case .live:
           value = (key as? any DependencyKey.Type)?.liveValue as? Key.Value
         case .preview:
-          #if canImport(SwiftUI)
-            if Thread.isPreviewAppEntryPoint {
-              return Key.previewValue
-            }
-          #endif
           if !CachedValues.isAccessingCachedDependencies {
             value = CachedValues.$isAccessingCachedDependencies.withValue(true) {
               return Key.previewValue

Sources/Dependencies/WithDependencies.swift
@@ -81,6 +81,11 @@ public func prepareDependencies<R>(
   _ updateValues: (inout DependencyValues) throws -> R
 ) rethrows -> R {
   var dependencies = DependencyValues._current
+  #if canImport(SwiftUI)
+    if Thread.isPreviewAppEntryPoint {
+      dependencies = DependencyValues()
+    }
+  #endif
   return try DependencyValues.$preparationID.withValue(UUID()) {
     #if DEBUG
       try DependencyValues.$isSetting.withValue(true) {

commit 6671c6c4463c143aa916e882e3753fbda9d05e4c

Fix Xcode preview regression introduced by #412 (#448)
* Fix `prepareDependencies` use in previews

* Simplify nesting

Sources/Dependencies/DependencyValues.swift
@@ -295,81 +295,79 @@ public struct DependencyValues: Sendable {
       return dependency
     }
     set {
-      if DependencyValues.isPreparing {
-        cachedValues.lock.lock()
-        defer { cachedValues.lock.unlock() }
-        let cacheKey = CachedValues.CacheKey(id: TypeIdentifier(key), context: context)
-        guard !cachedValues.cached.keys.contains(cacheKey) else {
-          if cachedValues.cached[cacheKey]?.preparationID != DependencyValues.preparationID {
-            if context != .preview {
-              #if DEBUG
-                reportIssue(
-                  {
-                    var dependencyDescription = ""
-                    if let fileID = DependencyValues.currentDependency.fileID,
-                      let line = DependencyValues.currentDependency.line
-                    {
-                      dependencyDescription.append(
-                        """
-                          Location:
-                            \(fileID):\(line)
-
-                        """
-                      )
-                    }
-                    dependencyDescription.append(
-                      Key.self == Key.Value.self
-                        ? """
-                          Dependency:
-                            \(typeName(Key.Value.self))
-                        """
-                        : """
-                          Key:
-                            \(typeName(Key.self))
-                          Value:
-                            \(typeName(Key.Value.self))
-                        """
-                    )
-                    var argument: String {
-                      "\(function)" == "subscript(key:)"
-                        ? "\(typeName(Key.self)).self"
-                        : "\\.\(function)"
-                    }
-                    return """
-                      @Dependency(\(argument)) has already been accessed or prepared.
-
-                      \(dependencyDescription)
-
-                      A global dependency can only be prepared a single time and cannot be \
-                      accessed beforehand. Prepare dependencies as early as possible in the \
-                      lifecycle of your application.
-
-                      To temporarily override a dependency in your application, use \
-                      'withDependencies' to do so in a well-defined scope.
-                      """
-                  }(),
-                  fileID: DependencyValues.currentDependency.fileID ?? fileID,
-                  filePath: DependencyValues.currentDependency.filePath ?? filePath,
-                  line: DependencyValues.currentDependency.line ?? line,
-                  column: DependencyValues.currentDependency.column ?? column
-                )
-              #endif
-            }
-          } else {
-            cachedValues.cached[cacheKey] = CachedValues.CachedValue(
-              base: newValue,
-              preparationID: DependencyValues.preparationID
-            )
-          }
-          return
-        }
-        cachedValues.cached[cacheKey] = CachedValues.CachedValue(
-          base: newValue,
-          preparationID: DependencyValues.preparationID
-        )
-      } else {
+      guard DependencyValues.isPreparing else {
         self.storage[ObjectIdentifier(key)] = newValue
+        return
       }
+      cachedValues.lock.lock()
+      defer { cachedValues.lock.unlock() }
+      let cacheKey = CachedValues.CacheKey(id: TypeIdentifier(key), context: context)
+      let existingValue = cachedValues.cached[cacheKey]
+      guard
+        existingValue == nil
+          || existingValue?.preparationID == DependencyValues.preparationID
+          || (context == .preview && existingValue?.preparationID == nil)
+      else {
+        if context != .preview {
+          #if DEBUG
+            reportIssue(
+              {
+                var dependencyDescription = ""
+                if let fileID = DependencyValues.currentDependency.fileID,
+                  let line = DependencyValues.currentDependency.line
+                {
+                  dependencyDescription.append(
+                    """
+                      Location:
+                        \(fileID):\(line)
+
+                    """
+                  )
+                }
+                dependencyDescription.append(
+                  Key.self == Key.Value.self
+                    ? """
+                      Dependency:
+                        \(typeName(Key.Value.self))
+                    """
+                    : """
+                      Key:
+                        \(typeName(Key.self))
+                      Value:
+                        \(typeName(Key.Value.self))
+                    """
+                )
+                var argument: String {
+                  "\(function)" == "subscript(key:)"
+                    ? "\(typeName(Key.self)).self"
+                    : "\\.\(function)"
+                }
+                return """
+                  @Dependency(\(argument)) has already been accessed or prepared.
+
+                  \(dependencyDescription)
+
+                  A global dependency can only be prepared a single time and cannot be \
+                  accessed beforehand. Prepare dependencies as early as possible in the \
+                  lifecycle of your application.
+
+                  To temporarily override a dependency in your application, use \
+                  'withDependencies' to do so in a well-defined scope.
+                  """
+              }(),
+              fileID: DependencyValues.currentDependency.fileID ?? fileID,
+              filePath: DependencyValues.currentDependency.filePath ?? filePath,
+              line: DependencyValues.currentDependency.line ?? line,
+              column: DependencyValues.currentDependency.column ?? column
+            )
+          #endif
+        }
+        return
+      }
+      cachedValues.cached[cacheKey] = CachedValues.CachedValue(
+        base: newValue,
+        preparationID: DependencyValues.preparationID
+      )
     }
   }
 

Tests/DependenciesTests/DependencyValuesTests.swift
@@ -870,6 +870,21 @@ final class DependencyValuesTests: XCTestCase {
     }
   #endif
 
+  #if DEBUG && !os(Linux) && !os(WASI) && !os(Windows)
+    func testPrepareDependencies_PreviewDependencyAccessBeforePrepare() {
+      withDependencies {
+        $0.context = .preview
+      } operation: {
+        @Dependency(\.date) var date
+        _ = date()
+        prepareDependencies {
+          $0.date = DateGenerator { Date(timeIntervalSinceReferenceDate: 42) }
+        }
+        XCTAssertEqual(date(), Date(timeIntervalSinceReferenceDate: 42))
+      }
+    }
+  #endif
+
   func testPrepareDependencies_setDependencyEndpoint() {
     prepareDependencies {
       $0[ClientWithEndpoint.self].get = { @Sendable in 42 }